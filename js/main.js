c910560f5d0e49173dd424b1e645e6dbadc8d6d0
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2022-07-16 00:23:27 +0200

    ostateczne

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function liczenie(num1, num2) {
+
+    if( (num1 == 20 && num2 == 20) || (num1 + num2 <= 20) ) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log ( liczenie(2,6));
